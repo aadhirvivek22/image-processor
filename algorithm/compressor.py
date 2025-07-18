a8f83794a61d80d4179feec068f8e150a46d24a6
aadhirvivek22 <aadhirvivekb@gmail.com>
2025-07-18 23:48:49 +0800

    Finalized compressor class and function to be used on the platform

--- algorithm/compressor.py
+++ algorithm/compressor.py
@@ -1,19 +1,25 @@
 from PIL import Image
-from utils import process_folder
+from .utils import process_folder
 
 class Compressor:
     def compress(self, input_path, output_path, output_format, quality, resize=None, strip_metadata=False): 
         image = Image.open(input_path)
-        self.save_image(image, output_path, output_format, quality)
+        print(quality)
+        print(output_format)
+        self.save_image(image, output_path, output_format=output_format, quality=quality)
 
 
     @staticmethod
     def save_image(image, output_path, output_format, quality):
-        image.save(output_path, quality=quality, format=output_format)
+        image.save(output_path, quality=quality, format=output_format, optimize=True)
     
 
 if __name__ == "__main__":
     paths = process_folder("dataset", "output")
     compressor = Compressor()
+    output_format = None
     for path in paths:
-        compressor.compress(path[0], path[1], "JPEG", 50, resize=(800, 600), strip_metadata=True)  
+        if not output_format:
+            output_format = path[2].upper() if (path[2] and path[2].upper()!="JPG") else "JPEG"
+        compressor.compress(path[0], path[1] + "." + path[2].lower(), output_format, 75)  
+        output_format = None
